7f3a2a7f8bda647b7dfc8b60f667bcbd9a403cd9
AnjanC0des <129589930+AnjanC0des@users.noreply.github.com>
2025-01-20 11:00:46 +0000

    remove problematic file

--- oldjavafiles/Consum.java
+++ oldjavafiles/Consum.java
@@ -1,7 +1,7 @@
 import java.util.function.Supplier;
 import java.util.function.Consumer;
 
-class Con{
+class Consum{
 	public static void main(String args[]){
 		Supplier<String> sup= ()-> "new String";
 		Consumer<String> con=(String s)-> System.out.println(s);
